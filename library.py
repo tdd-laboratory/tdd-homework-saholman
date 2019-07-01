34a94090f298a3855842137126f0500dd540086b
Spencer Holman <spencer.holman@evernym.com>
2019-07-01 14:14:02 -0600

    finish homework

--- library.py
+++ library.py
@@ -2,6 +2,8 @@ import re
 
 _whole_word = lambda x: re.compile(r'(?<=\W)' + x + '(?=\W)')
 _mixed_ordinal_pat = _whole_word(r'-?\d+(st|th|nd|rd)')
+_date_iso8601_pat = _whole_word(r'\d{4}-(0\d|1[0-2])-(0[1-9]|[12][0-9]|3[01])')
+_date_other_pat = _whole_word(r'\d{2} (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{4}')
 _integer_pat = _whole_word(r'\d+')
 _floating_point_after_pat = re.compile(r'\.\d+[^a-zA-Z.]')
 _floating_point_before_pat = re.compile(r'(?<=\d\.)')
@@ -10,6 +12,15 @@ def mixed_ordinals(text):
     '''Find tokens that begin with a number, and then have an ending like 1st or 2nd.'''
     for match in _mixed_ordinal_pat.finditer(text):
         yield('ordinal', match)
+
+def dates_iso8601(text):
+    for match in _date_iso8601_pat.finditer(text):
+        yield('date', match)
+
+def dates_other(text):
+    for match in _date_other_pat.finditer(text):
+        yield('date', match)
+
 
 def integers(text):
     '''Find integers in text. Don't count floating point numbers.'''
--- library_test.py
+++ library_test.py
@@ -7,7 +7,7 @@ began in 1845 (the twenty-second anniversary of the Mexican Revolution), and
 is the 1st example of a national independence holiday becoming popular in the
 Western Hemisphere. (The Fourth of July didn't see regular celebration in the
 US until 15-20 years later.) It is celebrated by 77.9% of the population--
-trending toward 80.                                                                
+trending toward 80.
 '''
 
 class TestCase(unittest.TestCase):
@@ -15,7 +15,7 @@ class TestCase(unittest.TestCase):
     # Helper function
     def assert_extract(self, text, extractors, *expected):
         actual = [x[1].group(0) for x in library.scan(text, extractors)]
-        self.assertEquals(str(actual), str([x for x in expected]))
+        self.assertEqual(str(actual), str([x for x in expected]))
 
     # First unit test; prove that if we scan NUM_CORPUS looking for mixed_ordinals,
     # we find "5th" and "1st".
@@ -25,11 +25,83 @@ class TestCase(unittest.TestCase):
     # Second unit test; prove that if we look for integers, we find four of them.
     def test_integers(self):
         self.assert_extract(NUM_CORPUS, library.integers, '1845', '15', '20', '80')
+ 
+    def test_integers_2(self):
+        self.assert_extract("I have 123,456,789 dollars", library.integers, '123,456,789')
 
     # Third unit test; prove that if we look for integers where there are none, we get no results.
     def test_no_integers(self):
         self.assert_extract("no integers", library.integers)
 
+    # Prove that dates successfully extracted
+    def test_dates_iso8601(self):
+        self.assert_extract('I was born on 2015-07-25.', library.dates_iso8601, '2015-07-25')
+
+    def test_bad_dates(self):
+        self.assert_extract('I was born on 2015-13-25.', library.dates_iso8601)
+        self.assert_extract('I was born on 2015-07-32.', library.dates_iso8601)
+
+    def test_new_date_format(self):
+        self.assert_extract('I was born on 25 Jan 2017.', library.dates_other, '25 Jan 2017')
+        # FIXME: Bad regex. Only works with period at the end
+
+    def test_new_date_format_2(self):
+        self.assert_extract('I was born on 25 Jan, 2017.', library.dates_other, '25 Jan, 2017')
+        # FIXME: Bad regex. Only works with period at the end
+
+    def test_third_date_format_1(self):
+        self.assert_extract('I was born on 2018-06-22 18:22:19.123.', library.dates_third, '2018-06-22 18:22:19.123')
+    def test_third_date_format_2(self):
+        self.assert_extract('I was born on 2018-06-22 18:22:19', library.dates_third, '2018-06-22 18:22:19')
+    def test_third_date_format_3(self):
+        self.assert_extract('I was born on 2018-06-22 18:22', library.dates_third, '2018-06-22 18:22')
+    def test_third_date_format_4(self):
+        self.assert_extract('I was born on 2018-06-22 18', library.dates_third, '2018-06-22 18')
+
+    def test_third_date_format_5(self):
+        self.assert_extract('I was born on 2018-06-22 18:22:19.123Z.', library.dates_third, '2018-06-22 18:22:19.123Z')
+
+    def test_third_date_format_6(self):
+        self.assert_extract('I was born on 2018-06-22 18:22:19Z.', library.dates_third, '2018-06-22 18:22:19Z')
+
+    def test_third_date_format_7(self):
+        self.assert_extract('I was born on 2018-06-22 18:22Z.', library.dates_third, '2018-06-22 18:22Z')
+    def test_third_date_format_8(self):
+        self.assert_extract('I was born on 2018-06-22 18Z.', library.dates_third, '2018-06-22 18Z')
+
+    def test_third_date_format_9(self):
+        self.assert_extract('I was born on 2018-06-22T18:22:19.123.', library.dates_third, '2018-06-22T18:22:19.123')
+
+    def test_third_date_format_10(self):
+        self.assert_extract('I was born on 2018-06-22T18:22:19', library.dates_third, '2018-06-22T18:22:19')
+    def test_third_date_format_11(self):
+        self.assert_extract('I was born on 2018-06-22T18:22', library.dates_third, '2018-06-22T18:22')
+    def test_third_date_format_12(self):
+        self.assert_extract('I was born on 2018-06-22T18', library.dates_third, '2018-06-22T18')
+
+    def test_third_date_format_13(self):
+        self.assert_extract('I was born on 2018-06-22T18:22:19.123Z.', library.dates_third, '2018-06-22T18:22:19.123Z')
+
+    def test_third_date_format_14(self):
+        self.assert_extract('I was born on 2018-06-22T18:22:19Z.', library.dates_third, '2018-06-22T18:22:19Z')
+
+    def test_third_date_format_15(self):
+        self.assert_extract('I was born on 2018-06-22T18:22Z.', library.dates_third, '2018-06-22T18:22Z')
+
+    def test_third_date_format_16(self):
+        self.assert_extract('I was born on 2018-06-22T18Z.', library.dates_third, '2018-06-22T18Z')
+
+    def test_third_date_format_17(self):
+        self.assert_extract('I was born on 2018-06-22T18:22:19.123MDT.', library.dates_third, '2018-06-22T18:22:19.123MDT')
+
+    def test_third_date_format_18(self):
+        self.assert_extract('I was born on 2018-06-22T18:22:19MDT', library.dates_third, '2018-06-22T18:22:19MDT')
+
+    def test_third_date_format_19(self):
+        self.assert_extract('I was born on 2018-06-22T18:22MDT.', library.dates_third, '2018-06-22T18:22MDT')
+
+    def test_third_date_format_20(self):
+        self.assert_extract('I was born on 2018-06-22T18MDT.', library.dates_third, '2018-06-22T18MDT')
 
 if __name__ == '__main__':
     unittest.main()
